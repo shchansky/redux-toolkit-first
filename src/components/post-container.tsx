cd8e169804b8645d27ea4da43c846b3966f86dca
shchansky <gavrilenko71285@gmail.com>
2023-02-02 19:21:58 +0300

    commit

--- src/components/post-container.tsx
+++ src/components/post-container.tsx
@@ -4,10 +4,12 @@ import { PostItem } from "./post-item";
 
 export const PostContainer = () => {
   /** useFetchAllPostsQuery_ автосгенерированные хуки на основании  описанных endpoint */
-  const { data: posts } = postAPI.useFetchAllPostsQuery(5);
+  const { data: posts, error, isLoading } = postAPI.useFetchAllPostsQuery(5);
 
   return (
     <div>
+      {isLoading && <h3>Идет загрузка...</h3>}
+      {error && <h3>Ошибка при загрузке...</h3>}
       {posts &&
         posts.map((post, index) => <PostItem post={post} key={post.id} />)}
     </div>
