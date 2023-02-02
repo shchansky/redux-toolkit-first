8d087d4c363c21739ae0a3490a613648069ac74e
shchansky <gavrilenko71285@gmail.com>
2023-02-02 18:41:11 +0300

    commit

--- src/services/post-service.ts
+++ src/services/post-service.ts
@@ -1,15 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 
-export const userAPI = createApi({
-  reducerPath: "userAPI",
+export const postAPI = createApi({
+  reducerPath: "postAPI",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://jsonplaceholder.typicode.com",
   }),
   endpoints: (build) => ({
     /** у build 2 метода:query для получения данных от сервера (GET запрос), mutation для изменения этих данных (POST и PUT запрос)  */
-    fetchAllUsers: build.query({
+    fetchAllPosts: build.query({
       query: () => ({
-        url: "/users",
+        url: "/posts",
       }),
     }),
   }),
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,16 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import useReducer from "./reducers/user-slice";
+import { postAPI } from "services/post-service";
 
 const rootReducer = combineReducers({
   useReducer,
+  [postAPI.reducerPath]: postAPI.reducer,
 });
 
 export const setupStore = () => {
   return configureStore({
     reducer: rootReducer,
+    middleware: (getDefaultMiddleware)=>getDefaultMiddleware().concat(postAPI.middleware)
   });
 };
 
